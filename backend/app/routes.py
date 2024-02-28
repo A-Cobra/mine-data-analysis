eb895e58dafe5c8517e6074f27d75b50acbaeb95
A-Cobra <conradbravina@gmail.com>
2024-02-28 15:54:26 -0500

    The analysis is made adequately

--- backend/app/routes.py
+++ backend/app/routes.py
@@ -1,10 +1,16 @@
-from flask import Blueprint, jsonify
+from flask import Blueprint, jsonify, request
+from app.utils.get_analysis_report import get_analysis_report
 
 main_bp = Blueprint('main', __name__)
 
 @main_bp.route('/api/v1/load', methods=['POST'])
 def home():
-    return jsonify({'message': 'Loaded successfully'})
+    data = request.get_json()
+    print("Loading")
+    print(data[0])
+
+    response = get_analysis_report(data)
+    return jsonify({'transformed': response})
 
 @main_bp.route('/api/v1/list', methods=['GET'])
 def about():
