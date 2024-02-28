cb3664c7e2655474364d4d3432d0c932eda6e1f5
A-Cobra <conradbravina@gmail.com>
2024-02-28 17:08:44 -0500

    Two methods work to save into the database

--- backend/app/controllers/analysis_controller.py
+++ backend/app/controllers/analysis_controller.py
@@ -1,11 +1,11 @@
 from ..config import get_connection
 
 
-def save_measurement_details(data_of_sensors):
+def save_measurement_details(measurement_details):
     connection = get_connection()
     cursor = connection.cursor()
     sql_query = "INSERT INTO measurement_detail (name_sensor, value, unit, timestamp) VALUES (%s, %s, %s, %s)"
-    for data_of_sensor in data_of_sensors:
+    for data_of_sensor in measurement_details:
         try:
             # Extract the values from the dictionary
             name_sensor = data_of_sensor["name_sensor"]
@@ -28,3 +28,42 @@ def save_measurement_details(data_of_sensors):
 
     # Close the cursor and the database connection
     cursor.close()
+
+
+def save_summary_details(summary):
+    connection = get_connection()
+    cursor = connection.cursor()
+    sql_query = "INSERT INTO summary (name_sensor, average, unit, timestamp) VALUES (%s, %s, %s, %s)"
+    for data_of_sensor in summary:
+        try:
+            # Extract the values from the dictionary
+            name_sensor = data_of_sensor["name_sensor"]
+            average = data_of_sensor["average"]
+            unit = data_of_sensor["unit"]
+            timestamp = data_of_sensor["timestamp"]
+
+            # Execute the INSERT statement with the values as parameters
+            cursor.execute(sql_query, (name_sensor, average, unit, timestamp))
+
+        except Exception as e:
+            # Handle the exception
+            print("An error occurred:", str(e))
+            connection.rollback()
+            cursor.close()
+            raise e
+
+    # Commit the transaction
+    connection.commit()
+
+    # Close the cursor and the database connection
+    cursor.close()
+
+
+def save_measurements_and_summary(measurement_details, summary):
+    connection = get_connection()
+    cursor = connection.cursor()
+
+    try:
+        pass
+    except:
+        pass
--- backend/app/routes.py
+++ backend/app/routes.py
@@ -1,6 +1,9 @@
 from flask import Blueprint, jsonify, request
 from app.utils.get_analysis_report import get_analysis_report
-from app.controllers.analysis_controller import save_measurement_details
+from app.controllers.analysis_controller import (
+    save_measurement_details,
+    save_summary_details,
+)
 
 main_bp = Blueprint("main", __name__)
 
@@ -12,9 +15,16 @@ def home():
     print(data[0])
 
     try:
-        save_measurement_details(data)
+        # save_measurement_details(data)
         response = get_analysis_report(data)
+        print("response")
+        print(response)
+        save_summary_details(response)
     except Exception as e:
+        print()
+        print()
+        print()
+        print(e)
         return jsonify({"msg": "failed"})
 
     return jsonify({"transformed": response})
