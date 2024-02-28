fa00e5220ce4b72d3968e3e291a35ecc50b47a58
A-Cobra <conradbravina@gmail.com>
2024-02-28 14:17:17 -0500

    Momentaneamente se acabo con el frontend

--- frontend/src/pages/Summary.jsx
+++ frontend/src/pages/Summary.jsx
@@ -1,7 +1,22 @@
-import React from 'react';
+import React, { useEffect } from 'react';
 
 const Summary = () => {
-  return <div>Summary</div>;
+  useEffect(() => {
+    const URL = '';
+    console.log('Fetching data');
+    // fetch(URL, { method: 'GET' })
+    //   .then((data) => {
+    //     console.log(data);
+    //   })
+    //   .catch((err) => {
+    //     console.log(err);
+    //   });
+  }, []);
+  return (
+    <>
+      <h2>Resumen</h2>
+    </>
+  );
 };
 
 export default Summary;
