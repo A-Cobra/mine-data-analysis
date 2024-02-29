0772ae5f3701d37702c18011f2d00a06778c5b8f
A-Cobra <conradbravina@gmail.com>
2024-02-29 09:38:37 -0500

    Docker compatibility between services works and everything is within one container

--- backend/app/config.py
+++ backend/app/config.py
@@ -4,9 +4,9 @@ import os
 
 def get_connection():
     return pymysql.connect(
-        host="127.0.0.1",
-        port=3800,
-        user=os.getenv("DB_USER"),
-        password=os.getenv("DB_PASSWORD"),
-        db=os.getenv("DB_NAME"),
+        host="database",
+        port=3306,
+        user="root",
+        password="",
+        db="mine",
     )
