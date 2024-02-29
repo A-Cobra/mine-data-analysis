eb8d4008570011f2e43492811713f532323b7eb8
A-Cobra <conradbravina@gmail.com>
2024-02-28 20:39:42 -0500

    The request of lisitng the summary is executed correctly
    Though with burned data, I need a date selector

--- frontend/src/pages/Summary.jsx
+++ frontend/src/pages/Summary.jsx
@@ -1,20 +1,28 @@
-import React, { useEffect } from 'react';
+import React, { useEffect, useState } from 'react';
 
 const Summary = () => {
+  const [summaryData, setSummaryData] = useState(null);
+
   useEffect(() => {
-    const URL = '';
-    console.log('Fetching data');
-    // fetch(URL, { method: 'GET' })
-    //   .then((data) => {
-    //     console.log(data);
-    //   })
-    //   .catch((err) => {
-    //     console.log(err);
-    //   });
+    const API_BASE_URL = 'http://127.0.0.1:5000';
+    const date = '2023-04-17';
+    const URL = `${API_BASE_URL}/api/v1/list/${date}`;
+
+    // console.log('Fetching data');
+    fetch(URL, { method: 'GET' })
+      .then(async (response) => {
+        const data = await response.json();
+        console.log(data);
+        setSummaryData(data);
+      })
+      .catch((err) => {
+        console.log(err);
+      });
   }, []);
   return (
     <>
       <h2>Resumen</h2>
+      <pre>{summaryData && JSON.stringify(summaryData)}</pre>
     </>
   );
 };
